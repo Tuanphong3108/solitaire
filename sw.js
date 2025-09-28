d9a34fbf8b6cef5fff3a0eaa76d490845c41a878
Dương Tuấn Phong <114390987+Tuanphong3108@users.noreply.github.com>
2025-09-28 13:51:39 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Định danh cache. CẦN THAY ĐỔI MỖI KHI CÓ BẢN CẬP NHẬT LỚN để kích hoạt xóa cache cũ.
-const CACHE_NAME = 'solitaire-m3-cache-v1.0.6'; 
+const CACHE_NAME = 'solitaire-m3-cache-v1.0.7'; 
 
 // Danh sách các file cốt lõi cần cache để chạy offline
 const urlsToCache = [
